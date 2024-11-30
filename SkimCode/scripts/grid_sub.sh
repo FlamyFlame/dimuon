18c740841074ccb1339061ad0f5d760ec313a977
Yuhan Guo <yuhang.guo@cern.ch>
2024-11-30 02:22:53 +0100

    added 24PbPb + 24pp to grid-job submission script

--- SkimCode/scripts/grid_sub.sh
+++ SkimCode/scripts/grid_sub.sh
@@ -1,11 +1,27 @@
-#2017 pp data
+#2024 pp data
 pathena TrigRates.py \
-        --inDS data17_5TeV.periodM.physics_Main.PhysCont.AOD.pro23_v02 \
-        --outDS user.yuhang.TrigRates.dimuon.pp2017data.May2024.2. \
-        --excludeFile=myfile_286411_r9582.root \
-        --excludeFile=myfile_286474_r11819.root \
+        --inDsTxt InDstxt_PP2024_5p36TeV.txt \
+        --outDS user.yuhang.TrigRates.dimuon.pp2024data.Nov2024.1. \
+        --excludeFile=myfile.root \
         --excludeFile=clean.sh \
         --excludeFile=vim_backup
+
+#2024 PbPb data
+# pathena TrigRates.py \
+#         --inDS data24_hi.physics_HardProbes.OpenEnded.AOD.t0pro41_v01 \
+#         --outDS user.yuhang.TrigRates.dimuon.PbPb2024data.Nov2024.1. \
+#         --excludeFile=myfile.root \
+#         --excludeFile=clean.sh \
+#         --excludeFile=vim_backup
+
+#2017 pp data
+# pathena TrigRates.py \
+#         --inDS data17_5TeV.periodM.physics_Main.PhysCont.AOD.pro23_v02 \
+#         --outDS user.yuhang.TrigRates.dimuon.pp2017data.May2024.2. \
+#         --excludeFile=myfile_286411_r9582.root \
+#         --excludeFile=myfile_286474_r11819.root \
+#         --excludeFile=clean.sh \
+#         --excludeFile=vim_backup
 
 #2015 pp data
 # pathena TrigRates.py \
@@ -15,8 +31,6 @@ pathena TrigRates.py \
 #         --excludeFile=myfile_286474_r11819.root \
 #         --excludeFile=clean.sh \
 #         --excludeFile=vim_backup
-
-
 
 ##2015 Pb+Pb data
 #pathena TrigRates_UPC2015.py \
