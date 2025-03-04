bcb06ac6bec348e50ea31c959193e6a66bbfc21a
Yuhan Guo <yuhan.guo@cern.ch>
2025-03-04 17:50:57 -0500

    moved all old scripts, obsolete or still potentially useful, into the old/ directory

--- Analysis/run_prt_gp_plots.sh
+++
@@ -1,4 +0,0 @@
-root -l -b -q mc_muon_pair_dp_ancestor_groups.c
-root -l -b -q mc_muon_pair_sp_ancestor_groups.c
-# root -l -b -q mc_muon_pair_parent_groups.c       
-root -l -b -q mc_muon_pair_same_prts.c
--- Analysis/run_scramble_pp.sh
+++
@@ -1,8 +0,0 @@
-root -b -l << EOF
-	.L ScrambGenPP.c
-	ScrambSampleGenPP pp;
-	pp.Run();
-
-	.q;
-
-EOF
