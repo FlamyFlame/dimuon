2f2b79f85336d1f4369a98c78ba405883b80f51d
Yuhan Guo <yuhan.guo@cern.ch>
2025-09-19 16:40:17 -0400

    update of setupATLAS

--- SkimCode/setup_21.sh
+++ SkimCode/setup_21.sh
@@ -1,3 +1,6 @@
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
+
 cd build_21
 acmSetup --sourcedir=../source_21 AthAnalysis,21.2.200
 cd ..
--- SkimCode/setup_24.sh
+++ SkimCode/setup_24.sh
@@ -1,4 +1,6 @@
-setupATLAS
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
+
 cd build_24
 acmSetup --sourcedir=../source_24 AthAnalysis,24.2.40
 cd ..
