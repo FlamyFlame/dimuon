9236db4da521450bee2cc162f45133eb0a3d90a2
Yuhan Guo <yuhan.guo@cern.ch>
2025-09-26 18:39:53 -0400

    add scripts to run Pb+Pb 23,24 data with no resonance cuts (for Z stats)

---
+++ Analysis/NTupleProcessingCode/run_pbpb_23_no_res_mu4_mu4noL1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The argument gets passed as the argument $(Process) in batch-submission script
+file_batch=$(( $1 + 1 ))
+
+cd $PWD
+
+# Setup ATLAS environment
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
+
+# Setup LCG
+lsetup "views LCG_107a_ATLAS_2 x86_64-el9-gcc13-opt"
+
+# Run the analysis
+root -b -l << EOF
+	.L PbPbDataNTupleFirstPass.c
+
+	PbPbDataNTupleFirstPass pbpb_23;
+	pbpb_23.run_year = 23;
+	pbpb_23.file_batch = $file_batch;
+ 	pbpb_23.resonance_cut_mode = 0;
+ 	pbpb_23.trigger_mode = 2;
+	pbpb_23.Run();
+
+	.q;
+
+EOF
+
