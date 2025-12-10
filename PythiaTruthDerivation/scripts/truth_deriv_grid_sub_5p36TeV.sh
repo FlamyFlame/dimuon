daf72509cb462ffcd3bdc332e0322576af9b798c
Yuhan Guo <yuhan.guo@cern.ch>
2025-12-10 15:17:39 -0500

    back up pythia truth derivation skimming script

---
+++ PythiaTruthDerivation/scripts/truth_deriv_grid_sub_5p36TeV.sh
@@ -0,0 +1,144 @@
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802758.Py8EG_A14_nn_hQCD_DiMu_pTH14_24.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH14_24.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802759.Py8EG_A14_nn_hQCD_DiMu_pTH125_300.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH125_300.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802760.Py8EG_A14_nn_hQCD_DiMu_pTH24_40.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH24_40.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802763.Py8EG_A14_nn_hQCD_DiMu_pTH8_14.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH8_14.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802777.Py8EG_A14_pp_hQCD_DiMu_pTH14_24.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH14_24.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802781.Py8EG_A14_pp_hQCD_DiMu_pTH8_14.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH8_14.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802761.Py8EG_A14_nn_hQCD_DiMu_pTH40_70.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH40_70.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802762.Py8EG_A14_nn_hQCD_DiMu_pTH70_125.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_nn_hQCD_DiMu_pTH70_125.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802764.Py8EG_A14_np_hQCD_DiMu_pTH125_300.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH125_300.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802776.Py8EG_A14_pp_hQCD_DiMu_pTH125_300.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH125_300.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802765.Py8EG_A14_np_hQCD_DiMu_pTH14_24.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH14_24.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802766.Py8EG_A14_np_hQCD_DiMu_pTH24_40.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH24_40.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802774.Py8EG_A14_pn_hQCD_DiMu_pTH70_125.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH70_125.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802767.Py8EG_A14_np_hQCD_DiMu_pTH40_70.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH40_70.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802768.Py8EG_A14_np_hQCD_DiMu_pTH70_125.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH70_125.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802769.Py8EG_A14_np_hQCD_DiMu_pTH8_14.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_np_hQCD_DiMu_pTH8_14.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802770.Py8EG_A14_pn_hQCD_DiMu_pTH125_300.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH125_300.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802771.Py8EG_A14_pn_hQCD_DiMu_pTH14_24.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH14_24.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802772.Py8EG_A14_pn_hQCD_DiMu_pTH24_40.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH24_40.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802773.Py8EG_A14_pn_hQCD_DiMu_pTH40_70.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH40_70.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802775.Py8EG_A14_pn_hQCD_DiMu_pTH8_14.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pn_hQCD_DiMu_pTH8_14.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802778.Py8EG_A14_pp_hQCD_DiMu_pTH24_40.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH24_40.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802779.Py8EG_A14_pp_hQCD_DiMu_pTH40_70.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH40_70.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
+pathena --trf "Derivation_tf.py --inputEVNTFile %IN --outputDAODFile output.pool.root --formats TRUTH0 --ignoreErrors True" \
+ 	--extOutFile DAOD_TRUTH0.output.pool.root \
+ 	--inDS mc23_5p36TeV:mc23_5p36TeV.802780.Py8EG_A14_pp_hQCD_DiMu_pTH70_125.evgen.EVNT.e8599 \
+ 	--outDS user.yuhang.DAOD.Pythia_5p36TeV_pp_hQCD_DiMu_pTH70_125.TRUTH0.17Sep2025.v1. \
+ 	--nFilesPerJob 8
+
