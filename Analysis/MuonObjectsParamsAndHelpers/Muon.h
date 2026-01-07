2a507d5374904713d2cf556d18504db8c717a04f
Yuhan <yuhan.guo@cern.ch>
2026-01-07 10:30:50 +0800

    template classes for Muon and MuonPair (UNTESTED)

--- Analysis/MuonObjectsParamsAndHelpers/Muon.h
+++ Analysis/MuonObjectsParamsAndHelpers/Muon.h
@@ -1,27 +1,50 @@
-#ifndef Muon_h
-#define Muon_h
+#pragma once
 #include <vector>
 
-class Muon{
-public:
-	float pt;
-   	float eta;
-   	float phi;
-   	float dP_overP;
-   	float z0;
-   	float d0;
-   	int ind;
-   	int charge;
-   	int quality;
-   	int ev_num;
-   	int ev_centrality;
-   	float ev_FCal_Et;
-	bool passmu4;
-
-
-   	Muon(){}
-   	~Muon(){}
+// -------------------- Muon helper structs (base & extras) --------------------
+struct MuonBase {
+  	float pt{-1e6}, eta{1e6}, phi{1e6};
+  	int   ind{-1}, ev_num{-1}, charge{0};
 };
 
+struct MuonData : MuonBase{
+   	float   dP_overP{1e6}, z0{1e6}, d0{1e6};
+   	int     quality{-1};
+	bool    passmu4{};
 
-#endif
+    float   trk_pt{-1e6}, trk_eta{1e6}, trk_phi{1e6};
+    int     trk_charge{};
+};
+
+struct MuonPbPbExtra{
+   	int ev_centrality{-1};
+   	float ev_FCal_Et{-1e6};
+};
+
+struct MuonPbPb: MuonData, MuonPbPbExtra{};
+
+struct MuonMCExtra {
+    float truth_pt{-1e6}, truth_eta{1e6}, truth_phi{1e6};
+    int   truth_bar{-1}, truth_ch{0};
+};
+
+struct MuonPythiaExtra {
+    // pythia-only fields (placeholder)
+};
+
+struct MuonPowhegExtra {
+    // powheg-only fields (placeholder)
+};
+
+struct MuonPythiaTruth : MuonBase, MuonMCExtra, MuonPythiaExtra {};
+struct MuonPowhegTruth : MuonBase, MuonMCExtra, MuonPowhegExtra {};
+
+using MuonPythia = MuonPythiaTruth;
+using MuonPowheg = MuonPowhegTruth;
+
+struct MuonPythiaFullSim : MuonData, MuonMCExtra, MuonPythiaExtra {};
+struct MuonPowhegFullSim : MuonData, MuonMCExtra, MuonPowhegExtra {};
+
+struct MuonPythiaFullSimOverlay : MuonData, MuonMCExtra, MuonPythiaExtra, MuonPbPbExtra {};
+struct MuonPowhegFullSimOverlay : MuonData, MuonMCExtra, MuonPowhegExtra, MuonPbPbExtra {};
+
