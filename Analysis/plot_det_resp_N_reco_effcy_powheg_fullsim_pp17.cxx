7359ab590e9cac2b2a592941ff50811b42fba287
Yuhan Guo <yg2695@columbia.edu>
2026-02-23 17:05:55 -0500

    add single-b 2D reco effcy plots

--- Analysis/plot_det_resp_N_reco_effcy_powheg_fullsim_pp17.cxx
+++ Analysis/plot_det_resp_N_reco_effcy_powheg_fullsim_pp17.cxx
@@ -1,4 +1,4 @@
-#include "DetRespPlotter.cxx"
+#include "RecoEffyRetRespPlotter.cxx"
 
 void plot_det_resp_N_reco_effcy_powheg_fullsim_pp17(){ // detector response & reco efficiencies
     DetRespPlotter pl;
